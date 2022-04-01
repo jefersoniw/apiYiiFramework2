92a56f7517f46758de204e1b1e1f2df8270cc03a
jefersoniw <jeferson_chagas25@hotmail.com>
2022-04-01 16:32:20 -0300

    Criando crud com gii, ajustando o arquivo de configuração web. Criando um modulo para api, com o gii também, definindo o modulo no web e declarando no defaultcontroller dentro do modulo api.

---
+++ modules/api/controllers/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use yii\rest\ActiveController;
+use yii\web\Controller;
+
+/**
+ * Default controller for the `api` module
+ */
+class DefaultController extends ActiveController
+{
+
+    public $modelClass = 'app\models\Noticia';
+}
---
+++ views/site/feed.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+  <div class="body-content">
+
+    <h1>Feed de Noticias via REST API</h1>
+
+  </div>
+</div>
